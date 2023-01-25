e9d418cc647bc2ddd95318916d26ade432e061e3
Juanes Paz <juanes.pazj@gmail.com>
2023-01-25 16:38:37 +0000

    upd

---
+++ JEapp/src/pltly.rs
@@ -0,0 +1,9 @@
+use plotly::{
+    color::{NamedColor},//, Rgb, Rgba
+    common::{Marker, Mode,Font,
+    //    ColorScale, ColorScalePalette, DashType, Fill,  Line, LineShape, 
+      //  Orientation, Title,
+    },
+    layout::{Axis,Shape,},//, BarMode, Layout, TicksDirection, TraceOrder},
+    Scatter,//Plot,BAR conflict
+};
--- tst/src/main.rs
+++ tst/src/main.rs
@@ -1,69 +1,157 @@
+#[macro_use]
+extern crate glium;
 
-use chrono::Datelike;
-use eframe::{
-    egui::{self, Color32},
-    epi,
-};
-use egui_datepicker::*;
-
-struct ExampleApp {
-    date: Date<Utc>,
-}
-
-impl Default for ExampleApp {
-    fn default() -> Self {
-        Self {
-            date: Utc::now().date(),
-        }
-    }
-}
-
-impl epi::App for ExampleApp {
-    fn name(&self) -> &str {
-        "Datepicker example"
-    }
-
-    fn update(&mut self, ctx: &egui::CtxRef, _frame: &epi::Frame) {
-        // ctx.set_debug_on_hover(true);
-        egui::CentralPanel::default().show(ctx, |ui| {
-            egui::Grid::new("exaamples_grid").show(ui, |ui| {
-                ui.label("Default");
-                ui.add(DatePicker::new("default", &mut self.date));
-                ui.end_row();
-                let mut date = Utc::now().date();
-                ui.label("Sunday first");
-                ui.add(DatePicker::new("sundayfirst", &mut date).sunday_first(true));
-                ui.end_row();
-                ui.label("Movable popup");
-                ui.add(DatePicker::new("movable", &mut self.date).movable(true));
-                ui.end_row();
-                ui.label("Different format");
-                ui.add(DatePicker::new("differentformat", &mut self.date).date_format(&"%d/%m/%Y"));
-                ui.end_row();
-                ui.label("Disable weekend highlight");
-                ui.add(
-                    DatePicker::new("noweekendhighlight", &mut self.date).highlight_weekend(false),
-                );
-                ui.end_row();
-                ui.label("Different weekend color");
-                ui.add(
-                    DatePicker::new("differentweekendcolor", &mut self.date)
-                        .highlight_weekend_color(Color32::from_rgb(0, 196, 0)),
-                );
-                ui.end_row();
-                ui.label("Different weekend days, i.e. holidays, Christmas, etc");
-                ui.add(
-                    DatePicker::new("differentweekenddays", &mut self.date)
-                        .weekend_days(|date| date.day() % 2 == 0),
-                );
-                ui.end_row();
-            });
-        });
-    }
-}
+#[path = "teapot.rs"]
+mod teapot;
 
 fn main() {
-    let app = ExampleApp::default();
-    let native_options = eframe::NativeOptions::default();
-    eframe::run_native(Box::new(app), native_options);
+    #[allow(unused_imports)]
+    use glium::{glutin, Surface};
+
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
+    let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
+    let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList,
+                                          &teapot::INDICES).unwrap();
+
+    let vertex_shader_src = r#"
+        #version 150
+        in vec3 position;
+        in vec3 normal;
+        out vec3 v_normal;
+        uniform mat4 perspective;
+        uniform mat4 view;
+        uniform mat4 model;
+        void main() {
+            mat4 modelview = view * model;
+            v_normal = transpose(inverse(mat3(modelview))) * normal;
+            gl_Position = perspective * modelview * vec4(position, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 150
+
+        in vec3 v_normal;
+        out vec4 color;
+        uniform vec3 u_light;
+
+        void main() {
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark_color = vec3(0.6, 0.0, 0.0);
+            vec3 regular_color = vec3(1.0, 0.0, 0.0);
+            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src,
+                                              None).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        let next_frame_time = std::time::Instant::now() +
+            std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        match event {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                _ => return,
+            },
+            glutin::event::Event::NewEvents(cause) => match cause {
+                glutin::event::StartCause::ResumeTimeReached { .. } => (),
+                glutin::event::StartCause::Init => (),
+                _ => return,
+            },
+            _ => return,
+        }
+
+        let mut target = display.draw();
+        target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
+
+        let model = [
+            [0.01, 0.0, 0.0, 0.0],
+            [0.0, 0.01, 0.0, 0.0],
+            [0.0, 0.0, 0.01, 0.0],
+            [0.0, 0.0, 2.0, 1.0f32]
+        ];
+
+        let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
+
+        let perspective = {
+            let (width, height) = target.get_dimensions();
+            let aspect_ratio = height as f32 / width as f32;
+
+            let fov: f32 = 3.141592 / 3.0;
+            let zfar = 1024.0;
+            let znear = 0.1;
+
+            let f = 1.0 / (fov / 2.0).tan();
+
+            [
+                [f *   aspect_ratio   ,    0.0,              0.0              ,   0.0],
+                [         0.0         ,     f ,              0.0              ,   0.0],
+                [         0.0         ,    0.0,  (zfar+znear)/(zfar-znear)    ,   1.0],
+                [         0.0         ,    0.0, -(2.0*zfar*znear)/(zfar-znear),   0.0],
+            ]
+        };
+
+        let light = [-1.0, 0.4, 0.9f32];
+
+        let params = glium::DrawParameters {
+            depth: glium::Depth {
+                test: glium::draw_parameters::DepthTest::IfLess,
+                write: true,
+                .. Default::default()
+            },
+            //backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
+            .. Default::default()
+        };
+
+        target.draw((&positions, &normals), &indices, &program,
+                    &uniform! { model: model, view: view, perspective: perspective, u_light: light },
+                    &params).unwrap();
+        target.finish().unwrap();
+    });
+}
+
+
+fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    let s = [up[1] * f[2] - up[2] * f[1],
+             up[2] * f[0] - up[0] * f[2],
+             up[0] * f[1] - up[1] * f[0]];
+
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    let u = [f[1] * s_norm[2] - f[2] * s_norm[1],
+             f[2] * s_norm[0] - f[0] * s_norm[2],
+             f[0] * s_norm[1] - f[1] * s_norm[0]];
+
+    let p = [-position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+             -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+             -position[0] * f[0] - position[1] * f[1] - position[2] * f[2]];
+
+    [
+        [s_norm[0], u[0], f[0], 0.0],
+        [s_norm[1], u[1], f[1], 0.0],
+        [s_norm[2], u[2], f[2], 0.0],
+        [p[0], p[1], p[2], 1.0],
+    ]
 }
